5c3f1f511220d2534ffbe0b4fc787208c1d2e3d5
Yanaty <tanyagta@bk.ru>
2019-07-24 22:15:38 +0300

    Add files via upload

---
+++ build/precache-manifest.0fcf6285ea6763d0d9d16f860f4fb1e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7e717b131ede2f539f5b98e3039873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490cac9449a840f91e4f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "01e55dfd9d3046d4613b",
+    "url": "/static/js/2.e2078c1d.chunk.js"
+  },
+  {
+    "revision": "490cac9449a840f91e4f",
+    "url": "/static/js/main.82b60ccd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
